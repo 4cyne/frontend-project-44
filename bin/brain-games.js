3d2cbe171e9489d4124d985fc569ded06b1a77bd
Ivan Kazakov <129972987+4cyne@users.noreply.github.com>
2025-05-07 17:01:43 +0300

    add eslint

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
 
-import { promptAndInsertName } from "../src/cli.js";
+import { promptAndInsertName } from '../src/cli.js'
 
-console.log("Welcome to the Brain Games!");
-console.log(`Hello, ${promptAndInsertName()}!`);
+console.log('Welcome to the Brain Games!')
+console.log(`Hello, ${promptAndInsertName()}!`)
