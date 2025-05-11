3e5956bc2334c7717b0a807adeabb5924567e83a
Ivan Kazakov <129972987+4cyne@users.noreply.github.com>
2025-05-11 17:35:25 +0300

    implementation of the brain-even game

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,6 +1,12 @@
 #!/usr/bin/env node
-
 import { promptAndInsertName } from '../src/cli.js'
 
-console.log('Welcome to the Brain Games!')
-console.log(`Hello, ${promptAndInsertName()}!`)
+const greeting = 'Welcome to the Brain Games!'
+const sayHello = `Hello, ${promptAndInsertName()}!`
+
+export const showGreeting = () => {
+  console.log(greeting)
+  console.log(sayHello)
+}
+
+showGreeting()
