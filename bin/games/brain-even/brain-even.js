129f4a071e7d99ffd0674e90ff395559ec4b5e34
Ivan Kazakov <129972987+4cyne@users.noreply.github.com>
2025-07-02 12:06:54 +0300

    text output edits

--- bin/games/brain-even/brain-even.js
+++ bin/games/brain-even/brain-even.js
@@ -3,7 +3,7 @@ import { getRandomNumber } from "../../utils/get-random-number.js";
 
 const isEven = (number) => number % 2 === 0;
 
-const gameRules = 'Answer "yes" if the number is even, otherwise answer "no".';
+const gameRules = "Answer 'yes' if the number is even, otherwise answer 'no'.";
 
 const game = () => {
   const number = getRandomNumber(1, 100);
