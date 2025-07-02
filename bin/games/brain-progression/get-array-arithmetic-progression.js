756f95b40348edaf47ede24c99fb80eafc97de48
Ivan Kazakov <129972987+4cyne@users.noreply.github.com>
2025-07-02 18:15:16 +0300

    update get-array-arithmetic-progression.js update logic getArray

--- bin/games/brain-progression/get-array-arithmetic-progression.js
+++ bin/games/brain-progression/get-array-arithmetic-progression.js
@@ -1,22 +1,23 @@
 import { getRandomNumber } from "../../utils/get-random-number.js";
 
 export const arithmeticProgression = (step, lim) => {
-  // получаем массив прогрессии
-  const arr = Array.from(
-    { length: Math.ceil(lim / step) },
-    (_, i) => (i + 1) * step
-  );
-  if (arr.length > 5 && arr.length < 10) {
-    // рандомно выбираем число, которое хотим скрыть
-    const randomIndex = Math.floor(getRandomNumber(1, arr.length - 1));
-    // сохраняем число
-    const hiddenNumber = arr[randomIndex];
-    arr[randomIndex] = "." + ".";
-    return {
-      progression: arr,
-      hiddenNumber,
-    };
-  } else {
-    arithmeticProgression(step, lim);
-  }
+  let arr;
+
+  do {
+    arr = Array.from(
+      { length: Math.ceil(lim / step) },
+      (_, i) => (i + 1) * step
+    );
+    if (arr.length < 5 || arr.length > 10) {
+      step = getRandomNumber(1, 10);
+      lim = getRandomNumber(20, 200);
+    }
+  } while (arr.length < 5 || arr.length > 10);
+  const randomIndex = Math.floor(getRandomNumber(1, arr.length - 1));
+  const hiddenNumber = arr[randomIndex];
+  arr[randomIndex] = "..";
+  return {
+    progression: arr,
+    hiddenNumber,
+  };
 };
