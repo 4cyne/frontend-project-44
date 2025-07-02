b59ade84f4d447a42abe245e393ccf78bbd82361
Ivan Kazakov <129972987+4cyne@users.noreply.github.com>
2025-07-02 17:32:32 +0300

    update get-array-arithmetic-progression.js arr.length < 10

--- bin/games/brain-progression/get-array-arithmetic-progression.js
+++ bin/games/brain-progression/get-array-arithmetic-progression.js
@@ -6,7 +6,7 @@ export const arithmeticProgression = (step, lim) => {
     { length: Math.ceil(lim / step) },
     (_, i) => (i + 1) * step
   );
-  if (arr.length > 5 && arr.length <= 10) {
+  if (arr.length > 5 && arr.length < 10) {
     // рандомно выбираем число, которое хотим скрыть
     const randomIndex = Math.floor(getRandomNumber(1, arr.length - 1));
     // сохраняем число
